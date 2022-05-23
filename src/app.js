1fa4f35ac683e42e13443bb5e8c66f07f460f9b8
asolare1 <asolare1@gmail.com>
2022-05-23 22:26:08 +0000

    done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function domaingenerator() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let f = 0; f < adj.length; f++) {
+        for (let g = 0; g < noun.length; g++) {
+          console.log(`${pronoun[i]}${adj[f]}${noun[g]}.com`);
+        }
+      }
+    }
+  }
+  domaingenerator();
 };
